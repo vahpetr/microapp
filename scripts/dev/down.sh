9652d524e5152fcbc2293e50330009eec6e9b3d2
Pyotr Vasilev <vahpetr@gmail.com>
2017-05-02 20:47:47 +0500

    fix down script

--- scripts/dev/down.sh
+++ scripts/dev/down.sh
@@ -2,10 +2,10 @@
 
 echo "Down begin"
 
-echo "Down cluster"
-sh scripts/dev/down-cluster.sh
-
 echo "Down app"
 sh scripts/dev/down-app.sh
+
+echo "Down cluster"
+sh scripts/dev/down-cluster.sh
 
 echo "Down end"
