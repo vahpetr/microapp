c93840865d30b3a0664200ed405563f08a39c737
Pyotr Vasilev <vahpetr@gmail.com>
2017-05-03 21:41:35 +0500

    controller crash fix

--- geolocation/src/main/java/com/microexample/geolocation/controllers/CoordinatesController.java
+++ geolocation/src/main/java/com/microexample/geolocation/controllers/CoordinatesController.java
@@ -5,10 +5,7 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
+import org.springframework.web.bind.annotation.*;
 
 import com.microexample.geolocation.contracts.ICoordinatesService;
 import com.microexample.geolocation.models.Coordinate;
@@ -26,8 +23,7 @@ public class CoordinatesController {
 
     @RequestMapping(method = RequestMethod.POST)
     @ResponseStatus(value = HttpStatus.CREATED)
-    public void post(@Valid @RequestBody Coordinate coordinate) {
+    public @ResponseBody void post(@Valid @RequestBody Coordinate coordinate) {
         _coordinatesService.save(coordinate);
     }
-
 }
